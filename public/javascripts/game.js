0390eb4363cbc3feda6815aa2540b83db588ac0a
William Van Cleve <vancleve@ksu.edu>
2020-10-27 18:36:01 -0500

    Add Lobby Info Button

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -9,16 +9,20 @@ $(document).ready(function () {
     gameCode.html(`Game Code: ${code}`);
 
     const startGame = $("#start-game-button");
-    startGame.click(function () {
-        if (!startGame.disabled) {
-            socket.emit('start-game');
-        }
-    });
+    if (startGame) {
+        startGame.click(function () {
+            if (!startGame.disabled) {
+                socket.emit('start-game');
+            }
+        });
+    }
 
     const closeGame = $("#close-game-button");
-    closeGame.click(function () {
-        socket.emit('close-lobby');
-    });
+    if (closeGame) {
+        closeGame.click(function () {
+            socket.emit('close-lobby');
+        });
+    }
 
     socket.on('update-players', currentPlayers => {
         $("#player-list").html(`
@@ -30,15 +34,21 @@ $(document).ready(function () {
         } else {
             startGame.removeClass("future-color-disabled");
         }
-    })
+    });
 
+    const lobbyInformation = $("#lobby-information")[0];
+    const gameInformation = $("#game-information");
     socket.on('start-game', gameHTML => {
-        const gameInformation = $("#game-information");
+        lobbyInformation.style.display = "none";
         gameInformation.html(gameHTML);
+
         if (!gameInformation.hasClass("active")) {
             gameInformation.addClass("active");
         }
-        startGame.html('Play Again');
+
+        if (startGame) {
+            startGame.html('Play Again');
+        }
     });
 
     socket.on('close-lobby', () => {
@@ -47,13 +57,24 @@ $(document).ready(function () {
 
     socket.emit('join-lobby', {name, code});
 
+    const openLobby = $("#lobby-open-button")[0];
+    openLobby.onclick = function() {
+        if (lobbyInformation.style.display == "none") {
+            lobbyInformation.style.display = "block";
+        } else {
+            lobbyInformation.style.display = "none";
+        }
+    }
+
     const lobby = $("#lobby")[0];
     const roles = $("#roles")[0];
+
     const openRoles = $("#roles-open-button")[0];
     openRoles.onclick = function() {
-        roles.style.display = "block";
         lobby.style.display = "none";
         openRoles.style.display = "none";
+        openLobby.style.display = "none";
+        roles.style.display = "block";
     }
 
     const closeRoles = $("#roles-close-button")[0];
@@ -61,5 +82,6 @@ $(document).ready(function () {
         roles.style.display = "none";
         lobby.style.display = "flex";
         openRoles.style.display = "block";
+        openLobby.style.display = "block";
     }
 });
