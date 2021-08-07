62fda8d2978c600ca3ee207722687be9a06a4d32
William Van Cleve <vancleve@ksu.edu>
2021-08-07 18:46:26 -0500

    Start of game log

--- routes/profile.js
+++ routes/profile.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const authenticate = require('../middleware/authenticate');
+const postgres = require('../models/database');
 
 /* GET home page. */
 router.get('/', authenticate, function(req, res) {
-  res.render('profile', { title: 'Profile', firstName: req.cookies.firstName, lastName: req.cookies.lastName });
+  const gameLogQuery = `select * from get_games(${req.cookies.userId})`;
+  postgres.query(gameLogQuery, (err, result) => {
+    let gameLog = null;
+    if (err) {
+        console.log(err.stack)
+    } else {
+      gameLog = result.rows;
+    }
+
+    res.render('profile', { title: 'Profile', firstName: req.cookies.firstName, lastName: req.cookies.lastName, gameLog: gameLog });
+  });
 });
 
 module.exports = router;
